8494854216b6336642fb3f7b64f4af344db5d125
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-07-09 13:28:51 -0500

    In GUI.h, created variables for height & width of the buttons, and wxButton & wxTextCtrl for the array of buttons and the display. Added the method for onButtonClick event, no code currently to activate it. Also declared Event Table.
    In GUI.cpp, added the space for event table, space for code for cosmetics (color, fonts, styles, etc.) and created new objects for the display and buttons. They work in that they exist in the program window, but the wxPoint & wxSize need to be adjusted to look correctly.
    
    Add code to display the grid of buttons, code will meed to be adjusted to look correctly.

--- Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
+++ Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
@@ -1,9 +1,30 @@
 #include "CalculatorGUI.h"
 
-CalculatorGUI::CalculatorGUI() : wxFrame(nullptr, wxID_ANY, "Lab 1.5 - Calculator", wxPoint(30, 30), wxSize(600, 800)) {
+wxBEGIN_EVENT_TABLE(CalculatorGUI, wxFrame)
+	//blank until begin assigning functions for diff buttons.
+wxEND_EVENT_TABLE()
 
+CalculatorGUI::CalculatorGUI() : wxFrame(nullptr, wxID_ANY, "Lab 1.5 - Calculator", wxPoint(400, 150), wxSize(600, 800)) {
+
+////Code for Cosmetics////
+
+////End Code for Cosmetics////
+
+	displayTextbox = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(450,150), wxSize(600, 250));
+	arrayOfButtons = new wxButton * [xWidth * yHeight];
+	wxGridSizer* grid = new wxGridSizer(xWidth, yHeight, 0, 0);
+	
+	for (int x = 0; x < xWidth; x++) {
+		for (int y = 0; y < yHeight; y++) {
+			arrayOfButtons[y * xWidth + x] = new wxButton(this, 1000 + (y * xWidth + x));
+			grid->Add(arrayOfButtons[y * xWidth + x], 1, wxEXPAND | wxALL);
+		}
+	}
+	this->SetSizer(grid);
+	grid->Layout();
 }
 
 CalculatorGUI::~CalculatorGUI()
 {
+	delete[] arrayOfButtons;
 }
--- Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.h
+++ Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.h
@@ -8,6 +8,13 @@ public:
 	~CalculatorGUI();
 
 public:
+	int xWidth = 4;
+	int yHeight = 6;
+	wxButton** arrayOfButtons;
+	wxTextCtrl* displayTextbox = nullptr;
 
+	void onButtonClick(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
 
