06ae901a889bcc0157d4e3bcd5394166f93273ec
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-07-09 17:39:10 -0500

    Began working with layout of the objects in the windows, still need adjustments; wrote out the button labels. Not sure how to get them to populate individually on the buttons yet.

--- Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
+++ Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
@@ -7,21 +7,24 @@ wxEND_EVENT_TABLE()
 CalculatorGUI::CalculatorGUI() : wxFrame(nullptr, wxID_ANY, "Lab 1.5 - Calculator", wxPoint(400, 150), wxSize(600, 800)) {
 
 ////Code for Cosmetics////
-
+	std::string buttonLabels[] = { "+/-", "0","1","2","3","4","5","6","7","8","9","=","+","-","*","/","mod","TBD","Hex","Dec","Bin","C" };
 ////End Code for Cosmetics////
 
-	displayTextbox = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(450,150), wxSize(600, 250));
+	displayTextbox = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(1,1), wxSize(590, 200));
 	arrayOfButtons = new wxButton * [xWidth * yHeight];
-	wxGridSizer* grid = new wxGridSizer(xWidth, yHeight, 0, 0);
+	wxGridSizer* grid = new wxGridSizer(yHeight, xWidth,0,0);
 	
 	for (int x = 0; x < xWidth; x++) {
 		for (int y = 0; y < yHeight; y++) {
 			arrayOfButtons[y * xWidth + x] = new wxButton(this, 1000 + (y * xWidth + x));
+			
 			grid->Add(arrayOfButtons[y * xWidth + x], 1, wxEXPAND | wxALL);
+			arrayOfButtons[y * xWidth + x]->SetAutoLayout(grid);
 		}
 	}
 	this->SetSizer(grid);
 	grid->Layout();
+	
 }
 
 CalculatorGUI::~CalculatorGUI()
