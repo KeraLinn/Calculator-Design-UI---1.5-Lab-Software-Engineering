4f77ff407e395167a36c7de03d769a369f7ab772
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-07-11 17:22:00 -0500

    Attempted a few different implementations to get the onButtonClick method to work. Instead of doing each EVT_BUTTON in the event table, I used the EVT_COMMAND_RANGE to specify the ID range for my buttons.
    Under the onButtonClick method, tried to do evt.getClientData or evt.getClientObject (and also getEventData and getEventObject) to display the button strings, but I couldn't figure out how to get the code to separate the individual evt commands. DisplayTextbox->AppendText(evt.GetClientData) and so forth didn't work.
    
    I didn't want to use switch cases, because that's a lot of extra code, but that's what I have implemented here so far to at least get them to execute properly. I think after I complete the next lab (ButtonFactory), then I will have the buttons in a better spot to be able to actually 'go through' them or call them directly to append to the displayTextbox.
    
    Have removed all previous code related to the arrayOfButtons that was commented out. Also created a region Button Creation to collapse that giant section of code.

--- Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
+++ Calculator Design UI - 1.5 Lab Software Engineering/CalculatorGUI.cpp
@@ -2,7 +2,9 @@
 
 
 wxBEGIN_EVENT_TABLE(CalculatorGUI, wxFrame)
-//blank until begin assigning functions for diff buttons.
+//EVT_BUTTON(100, CalculatorGUI::onButtonClick)
+//EVT_BUTTON(100-122,CalculatorGUI::onButtonClick)
+EVT_COMMAND_RANGE(100,123,wxEVT_COMMAND_BUTTON_CLICKED, CalculatorGUI::onButtonClick)
 wxEND_EVENT_TABLE()
 
 CalculatorGUI::CalculatorGUI() : wxFrame(nullptr, wxID_ANY, "Lab 1.5 - Calculator", wxPoint(400, 150), wxSize(500, 845)) {
@@ -18,56 +20,77 @@ CalculatorGUI::CalculatorGUI() : wxFrame(nullptr, wxID_ANY, "Lab 1.5 - Calculato
 	displayTextbox = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(0, 0), wxSize(500, 200));
 	displayTextbox->SetFont(calculatorDisplayFont);
 
+#pragma region Button Creation
+	ButtonNeg = new wxButton(this, 100, "+/-", wxPoint(0, 700), wxSize(125, 100));
+	Button0 = new wxButton(this, 101, "0", wxPoint(125, 700), wxSize(125, 100));
+	ButtonDot = new wxButton(this, 102, ".", wxPoint(250, 700), wxSize(125, 100));
+	ButtonEquals = new wxButton(this, 103, "=", wxPoint(375, 700), wxSize(125, 100));
 
-	ButtonNeg = new wxButton(this, 100, buttonLabels[0], wxPoint(0, 700), wxSize(125, 100));
-	Button0 = new wxButton(this, 101, buttonLabels[1], wxPoint(125, 700), wxSize(125, 100));
-	ButtonDot = new wxButton(this, 102, buttonLabels[2], wxPoint(250, 700), wxSize(125, 100));
-	ButtonEquals = new wxButton(this, 102, buttonLabels[3], wxPoint(375, 700), wxSize(125, 100));
+	Button1 = new wxButton(this, 104, "1", wxPoint(0, 600), wxSize(125, 100));
+	Button2 = new wxButton(this, 105, "2", wxPoint(125, 600), wxSize(125, 100));
+	Button3 = new wxButton(this, 106, "3", wxPoint(250, 600), wxSize(125, 100));
+	ButtonPlus = new wxButton(this, 107, "+", wxPoint(375, 600), wxSize(125, 100));
 
-	Button1 = new wxButton(this, 103, buttonLabels[4], wxPoint(0, 600), wxSize(125, 100));
-	Button2 = new wxButton(this, 104, buttonLabels[5], wxPoint(125, 600), wxSize(125, 100));
-	Button3 = new wxButton(this, 105, buttonLabels[6], wxPoint(250, 600), wxSize(125, 100));
-	ButtonPlus = new wxButton(this, 106, buttonLabels[7], wxPoint(375, 600), wxSize(125, 100));
+	Button4 = new wxButton(this, 108, "4", wxPoint(0, 500), wxSize(125, 100));
+	Button5 = new wxButton(this, 109, "5", wxPoint(125, 500), wxSize(125, 100));
+	Button6 = new wxButton(this, 110, "6", wxPoint(250, 500), wxSize(125, 100));
+	ButtonMinus = new wxButton(this, 111, "-", wxPoint(375, 500), wxSize(125, 100));
 
-	Button4 = new wxButton(this, 107, buttonLabels[8], wxPoint(0, 500), wxSize(125, 100));
-	Button5 = new wxButton(this, 108, buttonLabels[9], wxPoint(125, 500), wxSize(125, 100));
-	Button6 = new wxButton(this, 109, buttonLabels[10], wxPoint(250, 500), wxSize(125, 100));
-	ButtonMinus = new wxButton(this, 110, buttonLabels[11], wxPoint(375, 500), wxSize(125, 100));
-	
-	Button7 = new wxButton(this, 111, buttonLabels[12], wxPoint(0, 400), wxSize(125, 100));
-	Button8 = new wxButton(this, 112, buttonLabels[13], wxPoint(125, 400), wxSize(125, 100));
-	Button9 = new wxButton(this, 113, buttonLabels[14], wxPoint(250, 400), wxSize(125, 100));
-	ButtonMult = new wxButton(this, 114, buttonLabels[15], wxPoint(375, 400), wxSize(125, 100));
+	Button7 = new wxButton(this, 112, "7", wxPoint(0, 400), wxSize(125, 100));
+	Button8 = new wxButton(this, 113, "8", wxPoint(125, 400), wxSize(125, 100));
+	Button9 = new wxButton(this, 114, "9", wxPoint(250, 400), wxSize(125, 100));
+	ButtonMult = new wxButton(this, 115, "*", wxPoint(375, 400), wxSize(125, 100));
 
-	ButtonSquared = new wxButton(this, 115, buttonLabels[16], wxPoint(0, 300), wxSize(125, 100));
-	ButtonAbsVal = new wxButton(this, 116, buttonLabels[17], wxPoint(125, 300), wxSize(125, 100));
-	ButtonMod = new wxButton(this, 117, buttonLabels[18], wxPoint(250, 300), wxSize(125, 100));
-	ButtonDiv = new wxButton(this, 118, buttonLabels[19], wxPoint(375, 300), wxSize(125, 100));
-	
-	ButtonHex = new wxButton(this, 119, buttonLabels[20], wxPoint(0, 200), wxSize(125, 100));
-	ButtonDec = new wxButton(this, 120, buttonLabels[21], wxPoint(125, 200), wxSize(125, 100));
-	ButtonBin = new wxButton(this, 121, buttonLabels[22], wxPoint(250, 200), wxSize(125, 100));
-	ButtonClear = new wxButton(this, 122, buttonLabels[23], wxPoint(375, 200), wxSize(125, 100));
-	
-	
-	
-	//arrayOfButtons = new wxButton * [xWidth * yHeight];
-	//arrayOfButtons->set(this, wxID_ANY, " ",wxPoint(590, 225),wxSize();
-	/*wxGridSizer* grid = new wxGridSizer(yHeight, xWidth,0,0);
-	
-	for (int x = 0; x < xWidth; x++) {
-		for (int y = 0; y < yHeight; y++) {
-			arrayOfButtons[y * xWidth + x] = new wxButton(this, 1000 + (y * xWidth + x));
-			
-			grid->Add(arrayOfButtons[y * xWidth + x], 1, wxEXPAND | wxALL);
-			arrayOfButtons[y * xWidth + x]->SetAutoLayout(grid);
-		}
-	}
-	this->SetSizer(grid);
-	grid->Layout();*/
+	ButtonSquared = new wxButton(this, 116, "x^2", wxPoint(0, 300), wxSize(125, 100));
+	ButtonAbsVal = new wxButton(this, 117, "|x|", wxPoint(125, 300), wxSize(125, 100));
+	ButtonMod = new wxButton(this, 118, "mod (%)", wxPoint(250, 300), wxSize(125, 100));
+	ButtonDiv = new wxButton(this, 119, "/", wxPoint(375, 300), wxSize(125, 100));
+
+	ButtonHex = new wxButton(this, 120, "Hex", wxPoint(0, 200), wxSize(125, 100));
+	ButtonDec = new wxButton(this, 121, "Dec", wxPoint(125, 200), wxSize(125, 100));
+	ButtonBin = new wxButton(this, 122, "Bin", wxPoint(250, 200), wxSize(125, 100));
+	ButtonClear = new wxButton(this, 123, "C", wxPoint(375, 200), wxSize(125, 100));
+#pragma endregion
+
 }
 
 CalculatorGUI::~CalculatorGUI()
 {
-	//delete[] arrayOfButtons;
+	
 }
+
+void CalculatorGUI::onButtonClick(wxCommandEvent& evt) {
+	int id = evt.GetId();
+	wxString buttonLabels2[] = {"+/-", "0",".","=","1","2","3","+","4","5","6","-","7","8","9","*","x^2","|x|","mod (%)","/","Hex","Dec","Bin","C"};
+	switch (id) {
+	case 100: {displayTextbox->AppendText(buttonLabels2[0]); break; }
+	case 101: {displayTextbox->AppendText(buttonLabels2[1]); break; }
+	case 102: {displayTextbox->AppendText(buttonLabels2[2]); break; }
+	case 103: {displayTextbox->AppendText(buttonLabels2[3]); break; }
+	case 104: {displayTextbox->AppendText(buttonLabels2[4]); break; }
+	case 105: {displayTextbox->AppendText(buttonLabels2[5]); break; }
+	case 106: {displayTextbox->AppendText(buttonLabels2[6]); break; }
+	case 107: {displayTextbox->AppendText(buttonLabels2[7]); break; }
+	case 108: {displayTextbox->AppendText(buttonLabels2[8]); break; }
+	case 109: {displayTextbox->AppendText(buttonLabels2[9]); break; }
+	case 110: {displayTextbox->AppendText(buttonLabels2[10]); break; }
+	case 111: {displayTextbox->AppendText(buttonLabels2[11]); break; }
+	case 112: {displayTextbox->AppendText(buttonLabels2[12]); break; }
+	case 113: {displayTextbox->AppendText(buttonLabels2[13]); break; }
+	case 114: {displayTextbox->AppendText(buttonLabels2[14]); break; }
+	case 115: {displayTextbox->AppendText(buttonLabels2[15]); break; }
+	case 116: {displayTextbox->AppendText(buttonLabels2[16]); break; }
+	case 117: {displayTextbox->AppendText(buttonLabels2[17]); break; }
+	case 118: {displayTextbox->AppendText(buttonLabels2[18]); break; }
+	case 119: {displayTextbox->AppendText(buttonLabels2[19]); break; }
+	case 120: {displayTextbox->AppendText(buttonLabels2[20]); break; }
+	case 121: {displayTextbox->AppendText(buttonLabels2[21]); break; }
+	case 122: {displayTextbox->AppendText(buttonLabels2[22]); break; }
+	case 123: {displayTextbox->AppendText(buttonLabels2[23]); break; } //this should actually clear the screen, but displayTextBox->Clear() doesn't do it yet.
+	}
+
+}
+
+
+
+
