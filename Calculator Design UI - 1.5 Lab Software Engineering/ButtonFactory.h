55a0ad49541cabf9c31941240fc8255e6aeff765
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-07-13 11:57:56 -0500

    Added all Button creation text to the Button factory, including the ButtonFont. Removed ButtonLabels (under cosmetic code, lines 12-14), ButtonFont, and the original button creation code from CalculatorGUI.cpp. Put all the new buttonFactory code in CalculatorGUI.cpp into a region "Button Factory" to be able to collapse it.

--- Calculator Design UI - 1.5 Lab Software Engineering/ButtonFactory.h
+++ Calculator Design UI - 1.5 Lab Software Engineering/ButtonFactory.h
@@ -8,8 +8,31 @@ private:
 	wxWindowBase* _parentWindow;
 
 public:
+	
+	wxButton* CreateButtonNeg(CalculatorGUI* parentWindow); 
 	wxButton* CreateButton0(CalculatorGUI* parentWindow);
-
+	wxButton* CreateButtonDot(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonEquals(CalculatorGUI* parentWindow);
+	wxButton* CreateButton1(CalculatorGUI* parentWindow);
+	wxButton* CreateButton2(CalculatorGUI* parentWindow);
+	wxButton* CreateButton3(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonPlus(CalculatorGUI* parentWindow);
+	wxButton* CreateButton4(CalculatorGUI* parentWindow);
+	wxButton* CreateButton5(CalculatorGUI* parentWindow);
+	wxButton* CreateButton6(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonMinus(CalculatorGUI* parentWindow);
+	wxButton* CreateButton7(CalculatorGUI* parentWindow);
+	wxButton* CreateButton8(CalculatorGUI* parentWindow);
+	wxButton* CreateButton9(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonMult(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonSquared(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonAbsVal(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonMod(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonDiv(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonHex(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonDec(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonBin(CalculatorGUI* parentWindow);
+	wxButton* CreateButtonClear(CalculatorGUI* parentWindow);
 
 };
 
